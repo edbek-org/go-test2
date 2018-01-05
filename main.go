34013d713c98d2137593f5e1c5e67e3c3d487101
Eduard Shaidabekov <edbek@users.noreply.github.com>
2018-01-05 17:00:59 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"net/http"
+	"os"
+
+	"github.com/codegangsta/negroni"
 )
 
-
 func main() {
-	fmt.Fprint(w, "Hello World\n")
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	hostString := fmt.Sprintf(":%s", port)
+	n.Run(hostString)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "Hello from Go!")
 }
